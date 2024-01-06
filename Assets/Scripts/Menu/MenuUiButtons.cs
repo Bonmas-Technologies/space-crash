fc75b07fa905425ff333f1f15119b0d135608e0c
bonmas14 <bonmas14@gmail.com>
2024-01-06 17:43:12 +0300

    Revert "1.1 source"
    This reverts commit 9489626b175aea2e0f9d6b6f91862c30a100d250.

--- Assets/Scripts/Menu/MenuUiButtons.cs
+++ Assets/Scripts/Menu/MenuUiButtons.cs
@@ -1,36 +1,15 @@
-on sssss System.Net;
-using TMPro;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuUiButtons : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI _todoData;
-
-    private void Start()
-    {
-        if (_todoData == null)
-            return;
-
-        WebClient cl = new();
-
-        try
-        {
-            var todoHtml = cl.DownloadString("https://raw.githubusercontent.com/Bonmas-Technologies/space-crash/master/todo.txt"); // simple to-do list integration
-
-            _todoData.text = todoHtml;
-        }
-        catch (System.Exception)
-        {
-            _todoData.text = "Unable to connect for todo log";
-        }
-    }
 
     public void RunGame()
     {
         SceneManager.LoadScene(1);
     }
-
     public void GoToMenu()
     {
         SceneManager.LoadScene(0);
