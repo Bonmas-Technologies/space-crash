93004aecfbb5b1d09d7f330ebe77610dc92d54d8
bonmas14 <bonmas14@gmail.com>
2024-01-22 23:39:31 +0300

    1.2.2 билд индикатор

---
+++ Assets/Scripts/HudTargeter.cs
@@ -0,0 +1,58 @@
+using AI.API;
+using System;
+using UnityEngine;
+
+public class HudTargeter : MonoBehaviour
+{
+    [SerializeField] private GameObject _arrow;
+    [SerializeField] private GameObject _deadScreen;
+    [SerializeField] private MovementControl _player;
+
+    [SerializeField] private LayerMask _useLayer;
+
+    void Start()
+    {
+        _deadScreen.SetActive(false);
+        _arrow.SetActive(false);
+    }
+
+    void Update()
+    {
+        switch (_player.State)
+        {
+            case EntityState.Dead:
+                _deadScreen.SetActive(true);
+                _arrow.SetActive(false);
+                break;
+            case EntityState.Alive:
+                if (!_arrow.activeSelf)
+                    _arrow.SetActive(true);
+
+                SetAngleForArrow();
+                break;
+            case EntityState.InCar:
+                if (_arrow.activeSelf)
+                    _arrow.SetActive(false);
+                break;
+        }
+
+
+    }
+
+    private void SetAngleForArrow()
+    {
+        if (AiApi.GetClosestCarPosition(_player.transform.position, out Vector2 position, _useLayer.value))
+        {
+            Debug.Log("HERE");
+            position -= (Vector2)_player.transform.position;
+
+            float rotation = Mathf.Atan2(position.y, position.x) * Mathf.Rad2Deg;
+
+            _arrow.transform.rotation = Quaternion.Euler(0, 0, rotation - 90f);
+        }
+        else
+        {
+            _arrow.SetActive(false);
+        }
+    }
+}
